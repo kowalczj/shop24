080ff5f6be87170fac757927d689a2d1790a2a3f
JonPaulo <bautijon@oregonstate.edu>
2020-02-01 02:30:17 -0800

    add basic create/update/delete functionalities to product page
    Still needs some tidying up

--- app.py
+++ app.py
@@ -1,24 +1,74 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    product_name = db.Column(db.String(200), nullable=False)
+    product_description = db.Column(db.String(200), nullable=False)
+    product_price = db.Column(db.Float(10), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        name = request.form['product_name']
+        description = request.form['product_description']
+        price = request.form['product_price']
+
+        new_product = Todo(
+            product_name=name,
+            product_description=description,
+            product_price=price
+        )
+
+        try:
+            db.session.add(new_product)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error: There was a problem adding the new product data"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks = tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    product = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        product.product_name = request.form['product_name']
+        product.product_description = request.form['product_description']
+        product.product_price = request.form['product_price']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=product)
 
 @app.route("/vendors")
 def vendors():
